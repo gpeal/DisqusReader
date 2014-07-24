102dcf5c7044f48cb5384d173bcb37d8dbd857c1
Gabriel Peal <gpeal@google.com>
2014-07-23 17:55:32 -0700

    Initial commit

---
+++ app/src/main/java/reader/disqus/com/disqusreader/ReaderActivity.java
@@ -0,0 +1,16 @@
+package reader.disqus.com.disqusreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ReaderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reader_activity);
+    }
+}
